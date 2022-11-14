4ce42d536489ae0283ee067c9b114c4da20911ec
DzeiEm <julija.melesina@gmail.com>
2022-11-14 22:15:14 +0200

    .

--- topupbanking/topupbanking/Views/Home/HomeViewController.swift
+++ topupbanking/topupbanking/Views/Home/HomeViewController.swift
@@ -3,7 +3,16 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-
+    enum ButtonTitle: String {
+        case transfer = "Transfer money"
+        case history = "Transaction History"
+    }
+    
+    // MARK: - OUTLETS
+    @IBOutlet weak var historyButtonLabel: UIButton!
+    @IBOutlet weak var transferMoneyButtonLabel: UIButton!
+    
+    // MARK: - AUTLETS
     @IBAction func transactionHistoryButtonTapped() {
      
         print("Transactions button tapped")
@@ -14,4 +23,9 @@ class HomeViewController: UIViewController {
         print("Transfer money tapped")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        historyButtonLabel.titleLabel?.text = ButtonTitle.history.rawValue
+        transferMoneyButtonLabel.titleLabel?.text = ButtonTitle.transfer.rawValue
+    }
 }
